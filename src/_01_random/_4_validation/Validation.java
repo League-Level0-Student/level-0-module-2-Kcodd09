523f2c18eda86ff5414d70833bd8faf538f6850d
root <root@e07c050ab400>
2020-07-15 01:27:05 +0000

    working on random lottery #s

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,7 +17,24 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if(randomNumber==0) {
+			JOptionPane.showMessageDialog(null, "You are awesome!");
+		}
+		else if(randomNumber==1) {
+			JOptionPane.showMessageDialog(null, "You look great today!");
+		}
+		else if(randomNumber==2) {
+			JOptionPane.showMessageDialog(null, "You are super smart!");
+		}
+		else if(randomNumber==3) {
+			JOptionPane.showMessageDialog(null, "You rock!");
+		}
+		else if(randomNumber==4) {
+			JOptionPane.showMessageDialog(null, "You are so nice!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You are the best coder ever!");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,32 +6,41 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
+public static void main(String[] args) {
+	
 
 	// 2. Get the user to enter a question for the 8 ball to answer
-	
+	String question = JOptionPane.showInputDialog("Enter a question into the Magic 8 ball to reveal your fate.");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
-	
+	Random ran = new Random();
+	int answer = ran.nextInt(3);
 	// 4. If the random number is 0
-
+	if(answer==0) {
+		JOptionPane.showMessageDialog(null, "Yes");
+	}
 	// -- tell the user "Yes"
 
 	// 5. If the random number is 1
-
+	else if(answer==1) {
 	// -- tell the user "No"
-
+		JOptionPane.showMessageDialog(null, "No");
+	}
 	// 6. If the random number is 2
-
+	else if(answer==2) {
 	// -- tell the user "Maybe you should ask Google?"
-
+		JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+	}
 	// 7. If the random number is 3
-
+	else {
 	// -- write your own answer
+		JOptionPane.showMessageDialog(null, "Maybe...");
+}}}
 
-}
